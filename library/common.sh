66912bfb0acfa28b68c386f8c2dd276369c0f8e6
宋建昌 <songjianchang@troila.com>
2021-10-19 11:10:43 +0800

    test11111111111

--- library/common.sh
+++ library/common.sh
@@ -194,9 +194,18 @@ common::health_check(){
 common::run_kubespray(){
   : ${KUBESPRAY_IMAGE:=$(nerdctl images | awk '{print $1":"$2}' | grep '^kubespray:*' | sort -r --version-sort | head -n1)}
   nerdctl rm -f kubespray-runner >/dev/null 2>&1 || true
-  nerdctl run --rm -it --net=host --name kubespray-runner \
+  nerdctl run -d --net=host --name kubespray-runner \
   -v ${KUBESPRAY_CONFIG_DIR}:/kubespray/config \
   ${KUBESPRAY_IMAGE} $1
+  infolog "进入后台安装，可以ctrl-C退出"
+  infolog "进入后台安装，可以ctrl-C退出"
+  infolog "进入后台安装，可以ctrl-C退出"
+  sleep 3
+  infolog "如果log停止，可以自行退出重新输入'nerdctl logs -f kubespray-runne'查看日志"
+  infolog "如果log停止，可以自行退出重新输入'nerdctl logs -f kubespray-runne'查看日志"
+  infolog "如果log停止，可以自行退出重新输入'nerdctl logs -f kubespray-runne'查看日志"
+  sleep 3
+  nerdctl logs -f kubespray-runner
 }
 
 # Push kubespray image to registry
